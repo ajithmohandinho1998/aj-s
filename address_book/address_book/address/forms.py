5cea63b1ea100d47472c247acfbbfd5faf738c80
ajithmohandinho1998 <100209904+ajithmohandinho1998@users.noreply.github.com>
2022-04-16 16:10:09 +0530

    Delete address_book directory

--- address_book/address_book/address/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import address
-
-class addr_update(forms.ModelForm):
-    class Meta:
-        model=address
-        fields=['name','ad_l1','ad_l2','lat','long']
